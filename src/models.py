f618c6a4745d9145ae85cf23b2941bc3c60c6e02
jhonyes04 <jhonyes@hotmail.com>
2026-03-18 15:52:09 +0000

    Proyecto acabado

--- src/models.py
+++ src/models.py
@@ -5,20 +5,6 @@ import enum
 
 db = SQLAlchemy()
 
-# class User(db.Model):
-#     id: Mapped[int] = mapped_column(primary_key=True)
-#     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-#     password: Mapped[str] = mapped_column(nullable=False)
-#     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
-
-
-#     def serialize(self):
-#         return {
-#             "id": self.id,
-#             "email": self.email,
-#             # do not serialize the password, its a security breach
-#         }
-
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     username: Mapped[str] = mapped_column(
